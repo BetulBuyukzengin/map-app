464d25dcf08d8ba5cd2a49522682d47558f5e356
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-11-19 02:13:54 +0300

    feat: add map component

--- app/addLocation/page.tsx
+++ app/addLocation/page.tsx
@@ -1,7 +1,14 @@
-// import { MapContainer, TileLayer, useMap } from "react-leaflet";
+"use client";
+import "leaflet/dist/leaflet.css";
 
-function AddLocation() {
-  return <div>add location</div>;
-}
+import MapComponent from "../_components/MapComponent";
+
+const AddLocation = () => {
+  return (
+    <main className="relative">
+      <MapComponent />
+    </main>
+  );
+};
 
 export default AddLocation;

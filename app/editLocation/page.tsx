1ab87451916dd9f8b3bd7f871f883d086aeff007
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-11-19 02:14:21 +0300

    feat: add markers table

--- app/editLocation/page.tsx
+++ app/editLocation/page.tsx
@@ -1,5 +1,10 @@
+import MarkersTable from "../_components/MarkersTable";
 function EditLocation() {
-  return <div>edit location</div>;
+  return (
+    <div className="flex w-full justify-center items-center h-dvh">
+      <MarkersTable />
+    </div>
+  );
 }
 
 export default EditLocation;

3f1ff81e7c966b65f29508a86d012b01f0df8568
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-11-19 02:12:58 +0300

    style: edit style

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,9 +1,9 @@
-import Logo from "./Logo";
-import Navigation from "./Navigation";
+import Logo from "../_components/Logo";
+import Navigation from "../_components/Navigation";
 
 function Header() {
   return (
-    <header className=" px-8 py-5 bg-stone-600 shadow-md">
+    <header className="px-8 py-5 bg-stone-600 shadow-md fixed z-[1000] w-full">
       <div className="hidden md:flex  justify-between items-center max-w-7xl mx-auto">
         <Logo />
         <Navigation />

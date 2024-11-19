e2e80d050eab36837b505462bff944eb837c97d6
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-11-19 14:59:17 +0300

    feat: add table for list

--- app/listLocations/page.tsx
+++ app/listLocations/page.tsx
@@ -1,5 +1,18 @@
+"use client";
+import { useState } from "react";
+import MarkersTable from "../_components/MarkersTable";
+import { Button } from "@chakra-ui/react";
+import Link from "next/link";
 function ListLocations() {
-  return <div>List Locations</div>;
+  const [open, setOpen] = useState(false);
+  return (
+    <div className="flex flex-col w-full justify-center items-center h-dvh gap-5">
+      <Link href="/routeCreate">
+        <Button className="bg-slate-800 text-slate-200 px-4">Show Route</Button>
+      </Link>
+      <MarkersTable isList open={open} setOpen={setOpen} />
+    </div>
+  );
 }
 
 export default ListLocations;

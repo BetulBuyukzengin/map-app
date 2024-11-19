ef0ee54cbffe2b2b8bf25ba8b8f64dbffe96096a
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-11-19 14:57:20 +0300

    fix: fix home

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,9 @@
-import Image from "next/image";
-import map from "../public/map.jpg";
-
 export default function Home() {
   return (
-    <main className="mt-24">
-      <Image
-        src={map}
-        fill
-        placeholder="blur"
-        quality={80}
-        className="object-cover object-top opacity-60"
-        alt="map"
-      />
-      <div className="relative z-10 text-center">
-        <h1 className="text-2xl md:text-5xl text-stone-700 mb-10 tracking-tight font-serif">
-          Welcome to Map App!
-        </h1>
-      </div>
+    <main className="w-full h-dvh flex justify-center items-center">
+      <h1 className="text-2xl md:text-5xl text-slate-700 mb-10 tracking-tight font-semibold ">
+        Welcome to Map App!
+      </h1>
     </main>
   );
 }

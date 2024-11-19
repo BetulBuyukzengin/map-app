3b74a519cba5eb5743dd307848237d27f0ad6a77
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-11-19 15:02:38 +0300

    feat: create update marker func, fix add marker

--- app/_store/useMapStore.ts
+++ app/_store/useMapStore.ts
@@ -1,7 +1,6 @@
 import { LatLng } from "leaflet";
 import { create } from "zustand";
-import { v4 as uuidv4 } from "uuid";
-// StoreState arayüzünü tanımlayın
+
 interface StoreState {
   currentLocation: [number, number] | null;
   setCurrentLocation: (lat: number, lng: number) => void;
@@ -10,16 +9,31 @@ interface StoreState {
     address: string | null;
     color: string | null;
   }[];
+
   addMarker: ({
+    id,
     position,
     address,
     color,
     markerName,
   }: {
-    position: LatLng;
+    id: string;
+    position: [number, number];
+    // position: LatLng;
     address: string | null;
     color: string | null;
     markerName: string | null;
+  }) => void;
+  updateMarker: ({
+    id,
+    position,
+    color,
+    markerName,
+  }: {
+    id: string;
+    position?: [number, number];
+    color?: string | null;
+    markerName?: string | null;
   }) => void;
   error: string | null;
   setError: (message: string) => void;
@@ -28,19 +42,53 @@ interface StoreState {
 export const useMapStore = create<StoreState>((set) => ({
   currentLocation: null,
   error: null,
-  setError: (message) => set({ error: message }), // Hata mesajı için set fonksiyonu
+  setError: (message) => set({ error: message }),
   setCurrentLocation: (lat, lng) =>
     set({
       currentLocation: [lat, lng],
     }),
   markers: [],
-  addMarker: ({ position: { lat, lng }, markerName, color, address }) => {
-    console.log(markerName, lat, lng, color, address);
+  // addMarker: ({ position: { lat, lng }, markerName, color, address, id }) => {
+  //   return set((state) => ({
+  //     markers: [
+  //       ...state.markers,
+  //       { position: [lat, lng], address, color, markerName, id },
+  //     ],
+  //   }));
+  // },
+  addMarker: (marker) => {
     return set((state) => ({
-      markers: [
-        ...state.markers,
-        { position: [lat, lng], address, color, markerName, id: uuidv4() },
-      ],
+      markers: [...state.markers, marker],
+    }));
+  },
+  // updateMarker: ({ position: { lat, lng }, id, color, markerName }) => {
+  //   set((state) => ({
+  //     markers: state.markers.map((marker) => {
+  //       if (marker.id === id) {
+  //         return {
+  //           ...marker,
+  //           position: marker.position,
+  //           color: color ?? marker.color,
+  //           markerName: markerName ?? marker.markerName,
+  //         };
+  //       }
+  //       return marker;
+  //     }),
+  //   }));
+  // },
+  updateMarker: ({ id, position, color, markerName }) => {
+    set((state) => ({
+      markers: state.markers.map((marker) => {
+        if (marker.id === id) {
+          return {
+            ...marker,
+            position: position || marker.position,
+            color: color ?? marker.color,
+            markerName: markerName ?? marker.markerName,
+          };
+        }
+        return marker;
+      }),
     }));
   },
 }));

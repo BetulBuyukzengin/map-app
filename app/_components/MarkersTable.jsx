6845939cebf965d76eb2905554e41a74093a3509
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-11-19 15:04:40 +0300

    feat: conditionally render table for edit and list

--- app/_components/MarkersTable.jsx
+++ app/_components/MarkersTable.jsx
@@ -1,26 +1,83 @@
 "use client";
-import { Table } from "@chakra-ui/react";
+import { Button, Table } from "@chakra-ui/react";
+import { createIconWithColor } from "../_utils/createIconWithColor";
+import Link from "next/link";
+import CustomPopoverRoot from "./CustomPopoverRoot";
+import { useState } from "react";
+import { TiArrowRightThick } from "react-icons/ti";
+import { CustomDrawer } from "./CustomDrawer";
 import { useMapStore } from "../_store/useMapStore";
 
-const MarkersTable = () => {
-  //   const { markers } = useMapStore();
-  const markers = JSON.parse(localStorage.getItem("markers"));
-  console.log(markers);
+const MarkersTable = ({ isEdit, isList, setOpen, open }) => {
+  const { markers } = useMapStore();
+  const [openPopoverIndex, setOpenPopoverIndex] = useState(null);
+
+  const handlePopoverToggle = (index) => {
+    setOpenPopoverIndex((prevIndex) => (prevIndex === index ? null : index));
+  };
+
+  if (!markers || markers.length === 0)
+    return <div>Herhangi bir konum eklenmemiş</div>;
+
   return (
     <Table.Root size="sm" showColumnBorder className="w-[75%]">
       <Table.Header>
-        <Table.Row>
-          <Table.ColumnHeader>Location Name</Table.ColumnHeader>
-          <Table.ColumnHeader>Location Adderss</Table.ColumnHeader>
-          <Table.ColumnHeader textAlign="end">Color</Table.ColumnHeader>
-        </Table.Row>
+        {isList && (
+          <Table.Row>
+            <Table.ColumnHeader>Location Name</Table.ColumnHeader>
+            <Table.ColumnHeader>Marker Icon</Table.ColumnHeader>
+            <Table.ColumnHeader textAlign="end">{""}</Table.ColumnHeader>
+          </Table.Row>
+        )}
+        {isEdit && (
+          <Table.Row>
+            <Table.ColumnHeader>Location Name</Table.ColumnHeader>
+            <Table.ColumnHeader>Location Address</Table.ColumnHeader>
+            <Table.ColumnHeader textAlign="end">{""}</Table.ColumnHeader>
+          </Table.Row>
+        )}
       </Table.Header>
       <Table.Body>
-        {markers.map((marker) => (
+        {markers.map((marker, index) => (
           <Table.Row key={marker.id}>
-            <Table.Cell>{marker.markerName}</Table.Cell>
-            <Table.Cell>{marker.address}</Table.Cell>
-            <Table.Cell>{marker.color}</Table.Cell>
+            {isList && (
+              <>
+                <Table.Cell>{marker.markerName}</Table.Cell>
+                <Table.Cell className="flex">
+                  <Button onClick={() => handlePopoverToggle(index)}>
+                    <img
+                      src={createIconWithColor(marker.color).options.iconUrl}
+                      alt="Marker Icon"
+                      width={45}
+                      height={45}
+                    />
+                  </Button>
+                  {openPopoverIndex === index && (
+                    <CustomPopoverRoot open setOpen>
+                      <div>
+                        lat:{marker.position[0]} & lng: {marker.position[1]}
+                      </div>
+                    </CustomPopoverRoot>
+                  )}
+                </Table.Cell>
+                <Table.Cell>
+                  <Link href="/editLocation">
+                    <Button size="xs">
+                      <TiArrowRightThick />
+                    </Button>
+                  </Link>
+                </Table.Cell>
+              </>
+            )}
+            {isEdit && (
+              <>
+                <Table.Cell>{marker.markerName}</Table.Cell>
+                <Table.Cell>{marker.address}</Table.Cell>
+                <Table.Cell className="flex flex-col">
+                  <CustomDrawer markerId={marker.id} />
+                </Table.Cell>
+              </>
+            )}
           </Table.Row>
         ))}
       </Table.Body>

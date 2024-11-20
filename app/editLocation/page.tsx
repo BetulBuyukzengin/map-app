e39db9ae13d3c753feba5ca4438b99caa71d5c92
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-11-20 05:36:16 +0300

    fix: fix props, update style, add metadata

--- app/editLocation/page.tsx
+++ app/editLocation/page.tsx
@@ -1,8 +1,13 @@
 import MarkersTable from "../_components/MarkersTable";
+
+export const metadata = {
+  title: "Edit Location",
+};
+
 function EditLocation() {
   return (
-    <div className="flex w-full justify-center items-center h-dvh">
-      <MarkersTable isEdit />
+    <div className="flex w-full justify-center items-center h-dvh p-8">
+      <MarkersTable isEdit isList={false} />
     </div>
   );
 }
--- app/listLocations/page.tsx
+++ app/listLocations/page.tsx
@@ -1,16 +1,19 @@
 "use client";
-import { useState } from "react";
 import MarkersTable from "../_components/MarkersTable";
 import { Button } from "@chakra-ui/react";
 import Link from "next/link";
+import { useEffect } from "react";
 function ListLocations() {
-  const [open, setOpen] = useState(false);
+  useEffect(() => {
+    document.title = "List Locations";
+  }, []);
+
   return (
-    <div className="flex flex-col w-full justify-center items-center h-dvh gap-5">
+    <div className="flex flex-col w-full justify-center items-center h-dvh gap-5 p-8">
       <Link href="/routeCreate">
         <Button className="bg-slate-800 text-slate-200 px-4">Show Route</Button>
       </Link>
-      <MarkersTable isList open={open} setOpen={setOpen} />
+      <MarkersTable isList isEdit={false} />
     </div>
   );
 }

186423c112cf88d444f3b2b193fc2147cca5954d
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-11-20 06:53:58 +0300

    fix: fix bug for deploy

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,12 @@ const nextConfig: NextConfig = {
   },
   reactStrictMode: true,
   webpack: (config) => {
-    config.resolve.fallback = { fs: false, net: false, tls: false };
+    config.resolve.fallback = {
+      fs: false,
+      net: false,
+      tls: false,
+      path: false,
+    };
     return config;
   },
 };

db804c49c8ac9fb1cb89e78dea3d093642dc212a
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-11-20 05:31:18 +0300

    feat: add hamburger menu, style

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,13 +1,15 @@
 import Logo from "../_components/Logo";
 import Navigation from "../_components/Navigation";
+import HamburgerDrawerMenuContent from "./HamburgerDrawerMenuContent";
 
 function Header() {
   return (
     <header className="px-8 py-5 bg-slate-600 shadow-md fixed z-[1000] w-full">
-      <div className="hidden md:flex  justify-between items-center max-w-7xl mx-auto">
+      <div className="hidden lg:flex  justify-between items-center max-w-7xl xl:max-w-full mx-auto">
         <Logo />
         <Navigation />
       </div>
+      <HamburgerDrawerMenuContent />
     </header>
   );
 }

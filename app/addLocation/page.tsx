12398e2df2fae8f4fdfeaecb7f04a63b53f898df
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-11-20 06:22:36 +0300

    fix: fix bug for deploy

--- app/addLocation/page.tsx
+++ app/addLocation/page.tsx
@@ -1,12 +1,11 @@
 "use client";
 import "leaflet/dist/leaflet.css";
 import AddLocationsMapComponent from "../_components/AddLocationsMapComponent";
-import { useEffect } from "react";
 
 const AddLocation = () => {
-  useEffect(() => {
-    document.title = "Add Location";
-  }, []);
+  //   useEffect(() => {
+  //     document.title = "Add Location";
+  //   }, []);
 
   return (
     <main className="relative">

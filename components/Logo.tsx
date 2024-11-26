2399fea2df37deca8d3da5f5977990753c081e99
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-11-26 23:43:17 +0300

    style: fix style

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -2,15 +2,15 @@ import Link from "next/link";
 import { FaMapMarkedAlt } from "react-icons/fa";
 
 function Logo() {
-  return (
-    <Link
-      href="/"
-      className="flex flex-col md:flex-row items-center gap-4 z-10 text-stone-100"
-    >
-      <FaMapMarkedAlt className="size-28 lg:size-11" />
-      <span className="text-3xl font-serif">Map App</span>
-    </Link>
-  );
+    return (
+        <Link
+            href="/"
+            className="flex flex-col lg:flex-row items-center gap-4 z-10 text-stone-100"
+        >
+            <FaMapMarkedAlt className="size-28 lg:size-11" />
+            <span className="text-3xl font-serif">Map App</span>
+        </Link>
+    );
 }
 
 export default Logo;
